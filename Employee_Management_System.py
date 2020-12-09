64a8db7690ef861d624e5c7ca57a67aac3f2b378
cs50Javeria Shaikh <74369705+JaveriaShaikh@users.noreply.github.com>
2020-12-09 07:48:22 -0800

    Add files via upload

---
+++ Employee_Management_System.py
@@ -0,0 +1,6 @@
+print('Employee Management System')
+
+File = open('Employee_Management_System.txt' , 'w')
+
+File.write('Employee_Management_System \n 2012374 | 2012386 | 2012396 \n Store Employees Information using Python')
+File.close()
